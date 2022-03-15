630581a7f55db385713a795a6acf0226f3ef594a
saranya.sajeev <user123>
2022-03-15 14:37:56 +0530

    SerializeNulls added for moshi

--- lamp_kotlin/src/main/java/digital/lamp/lamp_kotlin/lamp_core/models/DimensionData.kt
+++ lamp_kotlin/src/main/java/digital/lamp/lamp_kotlin/lamp_core/models/DimensionData.kt
@@ -1,6 +1,6 @@
 package digital.lamp.lamp_kotlin.lamp_core.models
 
-import android.os.Build
+import digital.lamp.lamp_kotlin.lamp_core.infrastructure.SerializeNulls
 
 /**
  * Created by ZCO Engineering Dept. on 05,February,2020
@@ -29,71 +29,71 @@ data class DimensionData(
         val value: Any?,
         val type: Any?,
         val activity: ActivityData?,
-        var source:Any?,
+        var source: Any?,
         val duration: Long?
 )
 
 data class RotationData(
-    val x: Double?,
-    val y: Double?,
-    val z: Double?
+        val x: Double?,
+        val y: Double?,
+        val z: Double?
 )
 
 data class MotionData(
-    val x: Double?,
-    val y: Double?,
-    val z: Double?
+        val x: Double?,
+        val y: Double?,
+        val z: Double?
 )
 
 data class GravityData(
-    val x: Double?,
-    val y: Double?,
-    val z: Double?
+        val x: Double?,
+        val y: Double?,
+        val z: Double?
 )
 
 data class MagnetData(
-    val x: Double?,
-    val y: Double?,
-    val z: Double?
+        val x: Double?,
+        val y: Double?,
+        val z: Double?
 )
 
 data class NotificationData(
-    val action: String,
-    val user_action: String?,
-    val payload: String
+        val action: String,
+        val user_action: String?,
+        val payload: String
 )
 
 data class TokenData(
-    var action: String,
-    var device_token: String?,
-    var device_type: String,
-    var user_agent: String
-){
-    constructor() : this("",null,"", "")
+        var action: String,
+        var device_token: String?,
+        var device_type: String,
+        var user_agent: String
+) {
+    constructor() : this("", null, "", "")
 }
 
 data class ActivityData(
-    var running: Boolean?,
-    var cycling: Boolean?,
-    var automotive: Boolean?,
-    var stationary: Boolean?,
-    var unknown: Boolean?,
-    var walking: Boolean?,
-    var on_foot: Boolean?,
+        var running: Boolean?,
+        var cycling: Boolean?,
+        var automotive: Boolean?,
+        var stationary: Boolean?,
+        var unknown: Boolean?,
+        var walking: Boolean?,
+        var on_foot: Boolean?,
 ) {
-    constructor() : this(null,null,null,null,null,null,null)
+    constructor() : this(null, null, null, null, null, null, null)
 }
 
 data class BloodPressureData(
         val value: Float,
         val units: String?,
-        var source: Any?
+        @SerializeNulls var source: Any?
 )
 
 data class AttitudeData(
-    val x: Double?,
-    val y: Double?,
-    val z: Double?
+        val x: Double?,
+        val y: Double?,
+        val z: Double?
 )
 
 data class LowPowerModeData(
@@ -104,39 +104,39 @@ data class LowPowerModeData(
 )
 
 data class TelephonyData(
-    val duration: Int?,
-    val type: String?
+        val duration: Int?,
+        val type: String?
 )
 
 data class StepsData(
         val unit: String?,
         val value: Any?,
         val type: Any?,
-        var source:Any?,
+        @SerializeNulls var source: Any?,
 )
 
 data class NutritionData(
         val unit: String?,
         val value: Any?,
         val type: Any?,
-        var source:Any?,
+        @SerializeNulls var source: Any?,
 )
 
 data class GoogleFitData(
         val unit: String?,
         val value: Any?,
-        var source:Any?,
+        @SerializeNulls var source: Any?,
 )
 
 data class BloodPressure(
 
-val systolic: BloodPressureData?,
-val diastolic: BloodPressureData?,
+        val systolic: BloodPressureData?,
+        val diastolic: BloodPressureData?,
 )
 
 data class SleepData(
         val representation: String?,
         val value: Any?,
-        var source:Any?,
+        @SerializeNulls var source: Any?,
         val duration: Long?
 )
